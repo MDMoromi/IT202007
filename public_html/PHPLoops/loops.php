2a9bea1866ae4bc091f8182343ce21499e64e76b
MDMoromi <megadevmoromi@gmail.com>
2020-09-21 22:22:53 -0400

    Test 2

--- public_html/PHPLoops/loops.php
+++ public_html/PHPLoops/loops.php
@@ -1,6 +1,10 @@
 <?php
 	$array = array(2,3,4,5,6,7);
+	#foreach($array as $value){
+	#	echo "Value is $value <br/>";
+	#}
 	foreach($array as $value){
-		echo "Value is $value <br/>";
+		$check = $array[$value]/2;
+		echo "$check"
 	}
 ?>
