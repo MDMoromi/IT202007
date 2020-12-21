fa7d5fbd56e6e8c6266c1093859a12f74307ccc5
MDMoromi <megadevmoromi@gmail.com>
2020-12-20 22:21:11 -0500

    cart functionalityTM - fix? 2

--- public_html/shop.php
+++ public_html/shop.php
@@ -18,8 +18,8 @@ if(isset($_POST["product_id"])){
     if($result) {
         $name = $result["name"];
         $price = $result["price"];
-        $stmt = $db->prepare("INSERT INTO F20_Cart (user_id, product_id, price) VALUES(:user_id, :product_id, :price) ON DUPLICATE KEY UPDATE quantity = quantity +1, price = :price"); 
-		$r = $stmt->execute([":user_id"=>get_user_id(), ":product_id"=>$itemId, ":price"=>$price]); 
+        $stmt = $db->prepare("INSERT INTO Cart (user_id, product_id, price) VALUES(:user_id, :product_id, :price) ON DUPLICATE KEY UPDATE quantity = quantity +1, price = :price"); 
+		$r = $stmt->execute([":user_id"=>get_user_id(), ":product_id"=>$product_id, ":price"=>$price]); 
     }
 }
 ?>
