8c7cae92310a115e0f2c91e6750b9ee5df47c7d4
MDMoromi <megadevmoromi@gmail.com>
2020-12-20 19:09:48 -0500

    Shop Page Test

---
+++ public_html/shop.php
@@ -0,0 +1,11 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!is_logged_in()) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You must be logged in to access this page");
+    die(header("Location: login.php"));
+}
+
+<?php
+$query = "SELECT * FROM Products WHERE quantity > 0 ORDER BY CREATED DESC LIMIT 10";
+?>
