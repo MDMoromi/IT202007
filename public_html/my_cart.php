e40a39afc865d6c9ea9cd5f46aa3e44656aa2433
MDMoromi <megadevmoromi@gmail.com>
2020-12-21 21:34:54 -0500

    groundwork 3.1 fix

--- public_html/my_cart.php
+++ public_html/my_cart.php
@@ -82,7 +82,9 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <?php endif;?>
     </div>
 </div>
-<?php require(__DIR__ . "/partials/flash.php");
 <div>
 	<a class="checkout" href="checkout.php">Checkout</a>
 </div>
+
+	
+<?php require(__DIR__ . "/partials/flash.php");
